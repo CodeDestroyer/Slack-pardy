6be4057ab2f2ca1696798a2943e261ac6c6f90fb
Patrick Cunningham <pcunningham@Patricks-MacBook-Pro.local>
2015-07-16 11:14:44 -0400

    logging

--- app/Http/Middleware/TriggerWordFilter.php
+++ app/Http/Middleware/TriggerWordFilter.php
@@ -15,7 +15,16 @@ class TriggerWordFilter
      */
     public function handle($request, Closure $next)
     {
-        Log::debug($request->all());
+
+        if ($request->has('trigger_word') && $request->has('text')) {
+            Log::error("Im here");
+            $triggerWord = $request->get('trigger_word');
+            Log::warn($triggerWord);
+            $text = trim(str_replace($triggerWord,"",$request->get("text")));
+            Log::warn($text);
+            $request->merge(array('text' => $text));
+        }
+        Log::warn("didnt get in");
         return $next($request);
     }
 }
